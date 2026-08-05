d8e4d7287b1d2bf34fa6963dba2f95ff5f55851c
Arthur Albefaro <arthuralbefaroec@gmail.com>
2026-08-05 07:30:14 -0300

    chore(seo): bump the content last modified date

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -4,7 +4,7 @@
  */
 export const GITHUB_USERNAME = "arthuralbefaro";
 
-export const contentLastModified = "2026-08-04";
+export const contentLastModified = "2026-08-05";
 
 export const siteConfig = {
   name: "Arthur Albefaro",
